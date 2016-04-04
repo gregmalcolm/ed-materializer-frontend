231e0043ce88b7f71ad1164880d63cbb6ac95b47
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-04-03 21:58:15 -0400

    No https

--- config/environment.js
+++ config/environment.js
@@ -7,7 +7,7 @@ module.exports = function(environment) {
     baseURL: '/',
     locationType: 'auto',
     hostName: 'http://elasticbeanstalk-us-east-1-354643948167.s3-website-us-east-1.amazonaws.com',
-    apiHostName: 'https://ed-materializer-env.elasticbeanstalk.com',
+    apiHostName: 'http://ed-materializer-env.elasticbeanstalk.com',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
