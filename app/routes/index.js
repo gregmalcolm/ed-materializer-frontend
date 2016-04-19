f8157087b420a19c11cc736f0cc1431d7759c4ce
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-04-19 08:59:51 -0400

    Created a search widget in the top-nav

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,9 @@ import ApplicationRoute from './application';
 
 export default ApplicationRoute.extend({
   model() {
-    return this.store.query('survey', { per_page: 50,
-                                        sort: 'id',
-                                        direction: 'desc'});
+    return this.get("store").query('survey', { per_page: 50,
+                                               sort: 'id',
+                                               direction: 'desc'});
   }
+
 });
