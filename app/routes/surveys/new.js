c78540d388d0bc7657589c39d6954e960b86f9f7
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-04-23 23:45:04 -0400

    Componentized whole survey form

--- app/routes/surveys/new.js
+++ app/routes/surveys/new.js
@@ -2,9 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  session: Ember.inject.service('session'),
+
   model() {
     return {
-      survey: this.store.createRecord('survey')
+      survey: this.store.createRecord('survey', {
+        surveyed_by: this.get('session.data.authenticated.name'),
+      }),
     };
   },
 });
