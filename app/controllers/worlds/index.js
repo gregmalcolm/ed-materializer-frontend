b10eadbe503d887dcc80e9e9e2e840df1c84676c
Greg Malcolm (aka CMDR Marlon Blake) <greg@gregmalcolm.com>
2016-05-09 19:56:29 -0400

    Rearranging

--- app/controllers/worlds/index.js
+++ app/controllers/worlds/index.js
@@ -1,27 +1,20 @@
 import Ember from 'ember';
-//import SurveysController from '../surveys';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
-  //world: null,
+  world: null,
   systemName: null,
   worldName: null,
   queryParams: ['systemName', 'worldName'],
 
-  //fullSystem: Ember.computed('systemName', 'worldName', {
-    //get() {
-      //let systemName = this.get('systemName');
-      //let worldName = this.get('worldName');
-      //if (systemName && worldName) {
-        //return `${systemName} ${worldName}`;
-      //} else {
-        //return null;
-      //}
-    //}
-  //}),
 
   actions: {
+    newWorld() {
+
+    },
+    editWorld() {
+    },
     //newSurvey() {
       //this.transitionToRoute('worlds.system.new', this.get('systemName'),
                                                   //this.get('worldName'));
